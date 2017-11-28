c97d46285bcca4a419b9dd63d313ef20885b5cbc
Bertrand PRESLES <bertrand.presles@niji.fr>
2017-11-28 14:18:44 +0100

    Only execute setfacl commands on Linux

--- setup-dev-env.sh
+++ setup-dev-env.sh
@@ -6,6 +6,20 @@ cd "$( dirname "${BASH_SOURCE[0]}" )"
 
 source .env
 
+OS_INFORMATION="$(uname -s)"
+case "${OS_INFORMATION}" in
+    Linux*)     OS_NAME=linux;;
+    Darwin*)    OS_NAME=mac;;
+    CYGWIN*)    OS_NAME=windows;;
+    MINGW*)     OS_NAME=windows;;
+    *)          OS_NAME="UNKNOWN:${OS_INFORMATION}"
+esac
+
+if [ $OS_NAME == 'linux' ]; then
+    sudo setfacl -dR -m u:$(whoami):rwX -m u:82:rwX -m u:100:rX ./
+    sudo setfacl -R -m u:$(whoami):rwX -m u:82:rwX -m u:100:rX ./
+fi
+
 docker-compose -f docker-compose.yml -f docker-compose-dev.yml up -d
 
 docker-compose -f docker-compose.yml -f docker-compose-dev.yml exec -u root php sh -c "chmod -R a+w /var/www/html/web/sites/default"
