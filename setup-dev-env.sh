d2a635f3a9a6746c041e51ed9469c3c8e3526e65
Bertrand PRESLES <bertrand.presles@niji.fr>
2017-11-28 10:40:52 +0100

    removed setfacl commands as it's not supported on macOS and only works on Linux which is not a supported host OS at Niji

--- setup-dev-env.sh
+++ setup-dev-env.sh
@@ -6,12 +6,10 @@ cd "$( dirname "${BASH_SOURCE[0]}" )"
 
 source .env
 
-sudo setfacl -dR -m u:$(whoami):rwX -m u:82:rwX -m u:100:rX ./
-sudo setfacl -R -m u:$(whoami):rwX -m u:82:rwX -m u:100:rX ./
-
 docker-compose -f docker-compose.yml -f docker-compose-dev.yml up -d
 
-sudo chmod +w ./web/sites/default -R
+docker-compose -f docker-compose.yml -f docker-compose-dev.yml exec -u root php sh -c "chmod -R a+w /var/www/html/web/sites/default"
+docker-compose -f docker-compose.yml -f docker-compose-dev.yml exec -u root php sh -c "chmod -R a+w /tmp/cache"
 
 docker-compose exec php ./automation/bin/build.sh --mode dev
 
